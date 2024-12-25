20d83e2c8f60fc27165dc76cf404902c66be9b02
stevewalturn <steve@walturn.com>
2024-12-25 14:23:13 +0800

    Update files (batch 1/1)

---
+++ src/android/app/src/main/kotlin/com/digital/product/to/do/app/to/do/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digital_product.to_do_app.to_do_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ src/lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:to_do_app/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:to_do_app/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:to_do_app/features/home/home_view.dart';
+import 'package:to_do_app/features/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+// @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
